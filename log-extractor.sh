2d07401d9637633496eaeb6520184674bf73805b
hlibbabii <hlibbabii@gmail.com>
2018-03-11 22:24:37 +0100

    extractign lines before log statements

--- log-extractor.sh
+++ log-extractor.sh
@@ -1,9 +1,12 @@
 BASE_URL="https://github.com/apache/"
 LINE_PREFIX="#L"
 
+LINES_BEFORE_TO_EXTRACT=4
+
 REGEX='\([Ll]og\|LOG\)\.\([Tt]race\|[Dd]ebug\|[Ii]nfo\|[Ww]arn\|[Ee]rror\\[Ff]atal\)(.*)'
 
 FILE_FOR_OUTPUT=`pwd`/grepped_logs.`date "+%Y%m%d-%H%M%S"`
+echo ${LINES_BEFORE_TO_EXTRACT} >> ${FILE_FOR_OUTPUT}
 BASE_DIR=$1
 cd ${BASE_DIR}
 
@@ -19,7 +22,15 @@ for dir in *; do
 		LINE_NUMBER="$(echo $line | sed -n "s/^.*:\([0-9]*\):.*$/\1/p")"
 		LOG_STATEMENT="$(echo $line | sed -n "s/^.*:[0-9]*:\(.*\)$/\1/p")"
 
-		echo -e "${LOG_STATEMENT}\n${BASE_URL}${APACHE_PROJECT_NAME}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}\n" >> ${FILE_FOR_OUTPUT}
+		echo -e "${LOG_STATEMENT}\n${BASE_URL}${APACHE_PROJECT_NAME}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}" >> ${FILE_FOR_OUTPUT}
+		LINES_BEFORE_RANGE_START=$((LINE_NUMBER-LINES_BEFORE_TO_EXTRACT))
+		if [ ${LINES_BEFORE_RANGE_START} -lt "1" ]; then
+			LINES_BEFORE_RANGE_START="1"
+		fi
+
+		LINES_BEFORE_RANGE_END=$((LINE_NUMBER-1))
+		PREV_LINE=$(sed -n "${LINES_BEFORE_RANGE_START},${LINES_BEFORE_RANGE_END}p" ${FILE})
+		echo -e "${PREV_LINE}\n\n" >> ${FILE_FOR_OUTPUT}
 	done
 	cd ..
 done
