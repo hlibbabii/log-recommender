588b98aae3c56b8bc2d404208f5b0a555c8a48a9
hlibbabii <hlibbabii@gmail.com>
2018-03-13 00:53:47 +0100

    add check if file name was extracted

--- log-extractor.sh
+++ log-extractor.sh
@@ -20,19 +20,23 @@ for dir in *; do
 	echo grepping logs from ${APACHE_PROJECT_NAME} ...
 	grep -rn ${REGEX} | while read -r line ; do
 		FILE="$(echo $line | sed -n "s/^\(\S*\.\(java\|scala\|groovy\|py\|js\|c\|rb\)\).*$/\1/p")"
-		LINE_NUMBER="$(echo $line | sed -n "s/^.*:\([1-9][0-9]*\):.*$/\1/p")"
+		if [ -n "${FILE}" ]; then
+			LINE_NUMBER="$(echo $line | sed -n "s/^.*:\([1-9][0-9]*\):.*$/\1/p")"
 
-		echo "${BASE_URL}${APACHE_PROJECT_NAME}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}" >> ${FILE_FOR_OUTPUT}
-		LINES_RANGE_START=$((LINE_NUMBER-LINES_BEFORE_TO_EXTRACT))
-		while [ ${LINES_RANGE_START} -lt "1" ]; do
+			echo "${BASE_URL}${APACHE_PROJECT_NAME}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}" >> ${FILE_FOR_OUTPUT}
+			LINES_RANGE_START=$((LINE_NUMBER-LINES_BEFORE_TO_EXTRACT))
+			while [ ${LINES_RANGE_START} -lt "1" ]; do
+				echo "" >> ${FILE_FOR_OUTPUT}
+				LINES_RANGE_START=$((LINES_RANGE_START + 1))
+			done
+
+			LINES=$(sed -n "${LINES_RANGE_START},${LINE_NUMBER}p" ${FILE})
+			echo "${LINES}" >> ${FILE_FOR_OUTPUT}
 			echo "" >> ${FILE_FOR_OUTPUT}
-			LINES_RANGE_START=$((LINES_RANGE_START + 1))
-		done
-
-		LINES=$(sed -n "${LINES_RANGE_START},${LINE_NUMBER}p" ${FILE})
-		echo "${LINES}" >> ${FILE_FOR_OUTPUT}
-		echo "" >> ${FILE_FOR_OUTPUT}
-		echo "" >> ${FILE_FOR_OUTPUT}
+			echo "" >> ${FILE_FOR_OUTPUT}
+		else
+			echo "Can't extract file name: $line"
+		fi
 	done
 	cd ..
 done
