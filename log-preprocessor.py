8ab20e77895a72685da9205ac65a06b31698d8b8
hlibbabii <hlibbabii@gmail.com>
2018-03-12 18:27:01 +0100

    refactoring: renaming to underscore style

--- log-preprocessor.py
+++ log-preprocessor.py
@@ -21,21 +21,21 @@ def extract_text(line):
 def contains_text(line):
     return line.find("\"") >= 0
 
-def appendPeriodIfAbsent(line):
+def append_period_if_absent(line):
     if len(line) > 0:
         if line[-1] not in ['.', '!', '?']:
             line += "."
     return line
 
 
-def replaceStringResourcesNames(line):
+def replace_string_resources_names(line):
     changed = re.sub('^([0-9a-zA-Z]+\\.)+[0-9a-zA-Z]+$', '<STRING_RESOURCE>', line)
     if changed != line:
         print(line + "  ----->  " + changed)
     return changed
 
 
-def replaceVariablePlaceHolders(line):
+def replace_variable_place_holders(line):
     changed = re.sub('\\{\\}', '<VAR>', line)
     changed = re.sub('%[0-9]*[a-z]', '<VAR>', changed)
     if changed != line:
@@ -45,9 +45,9 @@ def replaceVariablePlaceHolders(line):
 
 def postprocess_extracted_text(line):
     line = line.strip()
-    line = replaceStringResourcesNames(line)
-    line = replaceVariablePlaceHolders(line)
-    line = appendPeriodIfAbsent(line)
+    line = replace_string_resources_names(line)
+    line = replace_variable_place_holders(line)
+    line = append_period_if_absent(line)
     return line
 
 
