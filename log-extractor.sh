7c099c7027cdfff6d59bdd4f31c382f021b49ce2
hlibbabii <hlibbabii@gmail.com>
2018-03-19 23:09:04 +0100

    change script to get projects from csv

--- log-extractor.sh
+++ log-extractor.sh
@@ -9,35 +9,45 @@ REGEX='\([Ll]og\|LOG\)\.\([Tt]race\|[Dd]ebug\|[Ii]nfo\|[Ww]arn\|[Ee]rror\\[Ff]at
 FILE_FOR_OUTPUT=`pwd`/grepped_logs.`date "+%Y%m%d-%H%M%S"`
 echo "Extracting logs to ${FILE_FOR_OUTPUT}"
 echo ${LINES_BEFORE_TO_EXTRACT} >> ${FILE_FOR_OUTPUT}
-BASE_DIR=$1
-cd ${BASE_DIR}
 
-for dir in *; do
-	cd ${dir}
-	PWD=`pwd`
-	APACHE_PROJECT_NAME=`basename "${PWD}"`
-	COMMIT_HASH=`git log -n 1 --pretty=format:"%H"`
+CSV_FILE=$1
+echo "Getting projects from $CSV_FILE"
 
-	echo grepping logs from ${APACHE_PROJECT_NAME} ...
-	grep -rn ${REGEX} | while read -r line ; do
-		FILE="$(echo $line | sed -n "s/^\(\S*\.\(java\|scala\|groovy\|py\|js\|c\|rb\|adoc\|md\|vm\)\).*$/\1/p")"
-		if [ -n "${FILE}" ]; then
-			LINE_NUMBER="$(echo $line | sed -n "s/^.*:\([1-9][0-9]*\):.*$/\1/p")"
+#BASE_DIR=$2
+#cd ${BASE_DIR}
 
-			echo "${BASE_URL}${APACHE_PROJECT_NAME}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}" >> ${FILE_FOR_OUTPUT}
-			LINES_RANGE_START=$((LINE_NUMBER-LINES_BEFORE_TO_EXTRACT))
-			while [ ${LINES_RANGE_START} -lt "1" ]; do
-				echo "" >> ${FILE_FOR_OUTPUT}
-				LINES_RANGE_START=$((LINES_RANGE_START + 1))
-			done
+while IFS=, read -r PROJECT_NAME PROJECT_LINK
+do
+    if [ -d "${PROJECT_NAME}" ]; then
+         echo "${PROJECT_NAME} already exists"
+    else
+        echo "Getting ${PROJECT_NAME}"
+        git clone ${PROJECT_LINK} ${PROJECT_NAME}
+    fi
+    cd ${PROJECT_NAME}
+    COMMIT_HASH=`git log -n 1 --pretty=format:"%H"`
 
-			LINES=$(sed -n "${LINES_RANGE_START},${LINE_NUMBER}p" ${FILE})
-			echo "${LINES}" >> ${FILE_FOR_OUTPUT}
-			echo "" >> ${FILE_FOR_OUTPUT}
-			echo "" >> ${FILE_FOR_OUTPUT}
-		else
-			echo "Can't extract file name: $line"
-		fi
-	done
-	cd ..
-done
+    echo grepping logs from ${PROJECT_NAME} ...
+    grep -rn ${REGEX} | while read -r line ; do
+        FILE="$(echo $line | sed -n "s/^\(\S*\.\(java\|scala\|groovy\|py\|js\|c\|rb\|adoc\|md\|vm\)\).*$/\1/p")"
+        if [ -n "${FILE}" ]; then
+            LINE_NUMBER="$(echo $line | sed -n "s/^.*:\([1-9][0-9]*\):.*$/\1/p")"
+            BASE_PROJECT_URL="$(echo $PROJECT_LINK | sed -n "s/^\(git\)\(.*\)\.git$/https\2/p")"
+
+            echo "${BASE_PROJECT_URL}/blob/${COMMIT_HASH}/${FILE}${LINE_PREFIX}${LINE_NUMBER}" >> ${FILE_FOR_OUTPUT}
+            LINES_RANGE_START=$((LINE_NUMBER-LINES_BEFORE_TO_EXTRACT))
+            while [ ${LINES_RANGE_START} -lt "1" ]; do
+                echo "" >> ${FILE_FOR_OUTPUT}
+                LINES_RANGE_START=$((LINES_RANGE_START + 1))
+            done
+
+            LINES=$(sed -n "${LINES_RANGE_START},${LINE_NUMBER}p" ${FILE})
+            echo "${LINES}" >> ${FILE_FOR_OUTPUT}
+            echo "" >> ${FILE_FOR_OUTPUT}
+            echo "" >> ${FILE_FOR_OUTPUT}
+        else
+            echo "Can't extract file name: $line"
+        fi
+    done
+    cd ..
+done < ${CSV_FILE}
