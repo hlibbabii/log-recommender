65ffb6b422530a0ab80c1ca6f71c6d7a329edc52
hlibbabii <hlibbabii@gmail.com>
2018-04-22 08:26:43 +0200

    make possible to filter out 1 and 2 char tokens, add some 1 and 2 character tokens,temporarily using only context before

--- log_statement.py
+++ log_statement.py
@@ -14,7 +14,8 @@ class LogStatement(object):
         self.n_variables = n_variables
         self.context_before = context_before
         self.context_after = context_after
-        self.context_words = preprocess_context(self.context_before + self.context_after)
+        # self.context_words = preprocess_context(self.context_before + self.context_after)
+        self.context_words = preprocess_context(self.context_before)
         self.project = project
         self.link = link
 
@@ -22,6 +23,8 @@ class LogStatement(object):
         return self.log_text_words[0] if len(self.log_text_words) > 0 else ""
 
 two_character_tokens = [
+    "/*",
+    "*/",
     "==",
     "!=",
     "**",
@@ -39,13 +42,22 @@ two_character_tokens = [
     "||"
 ]
 
-def two_character_tokens_regex():
-    m = list(map(lambda x: x.replace("+", "\+").replace("|", "\|").replace("*", "\*"), two_character_tokens))
+one_character_tokens = [
+    "+",
+    "*",
+    "!",
+    "/",
+    ">",
+    "<",
+    "="
+]
+
+def create_regex_from_token_list(token_list):
+    m = list(map(lambda x: x.replace("+", "\+").replace("|", "\|").replace("*", "\*"), token_list))
     return "(" + "|".join(
         m
     ) +")"
 
-one_character_tokens = "(=|\+|\*|!|/|>|<)"
 
 delimiters_to_drop = "[\[\] ,.\-?:\n\t(){};\"&|_#\\\@$]"
 
@@ -55,20 +67,29 @@ def camel_case_split(identifier):
     return [m.group(0) for m in matches]
 
 def split_to_key_words_and_identifiers(line):
-    regex = two_character_tokens_regex()
+    regex = create_regex_from_token_list(two_character_tokens)
     two_char_tokens_separated = re.split(regex, line)
     result =[]
     for str in two_char_tokens_separated:
         if str in two_character_tokens:
             result.append(str)
         else:
-            one_char_token_separated = re.split(one_character_tokens, str)
+            one_char_token_separated = re.split(create_regex_from_token_list(one_character_tokens), str)
             result.extend(list(filter(None, itertools.chain.from_iterable(
                 [re.split(delimiters_to_drop, str) for str in one_char_token_separated]
             ))))
     return result
 
+FILTER_OUT_1_AND_2_CHAR_TOKENS = True
+
+
+def filter_out_1_and_2_char_tokens(context):
+    return list(filter(lambda x: x not in one_character_tokens and x not in two_character_tokens, context))
+
+
 def preprocess_context(context):
     context = split_to_key_words_and_identifiers(context)
+    if FILTER_OUT_1_AND_2_CHAR_TOKENS:
+        context = filter_out_1_and_2_char_tokens(context)
     context = [item.lower() for identifier in context for item in camel_case_split(identifier)]
     return context
