c76ebbb92d1e3b3e858c0ea4731338c762749f73
hlibbabii <hlibbabii@gmail.com>
2018-06-14 02:29:19 +0200

    add mrr_metric and learning rate finding mode switch

--- nn/params.py
+++ nn/params.py
@@ -4,12 +4,12 @@ PATH='../nn-data'
 
 # bs=32
 bs=64
-bptt=40
-em_sz = 70  # size of each embedding vector
-nh = 100     # number of hidden activations per layer
+bptt=70
+em_sz = 300  # size of each embedding vector
+nh = 400     # number of hidden activations per layer
 nl = 3       # number of layers
 
 TEXT = data.Field()
 LEVEL_LABEL = data.Field(sequential=False)
 
-pretrained_lang_model_name='tst'
+pretrained_lang_model_name='devanbu_no_replaced_identifier_split'
