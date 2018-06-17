3d43be21a78f21356d53dde8df7e323bbd5fba56
hlibbabii <hlibbabii@gmail.com>
2018-06-17 21:57:11 +0200

    add metrics as params

--- nn/lang_model.py
+++ nn/lang_model.py
@@ -12,42 +12,61 @@ from functools import partial
 from fastai.core import USE_GPU
 from fastai.metrics import accuracy, top_k, MRR, mrr_non_interactive
 from fastai.nlp import LanguageModelData, seq2seq_reg
-from params import TEXT, nn_params, Mode
+from params import TEXT, Mode, nn_params
 from utils import to_test_mode, gen_text, back_to_train_mode, f2, beautify_text
 import dill as pickle
 
 logging.basicConfig(level=logging.DEBUG)
 
+nn_arch = nn_params['arch']
 
-def calculate_and_display_metrics(text_field):
-    accuracies, examples = top_k(*rnn_learner.predict_with_targs(True), [1, 10, 100], 2)
-
+def display_not_guessed_examples(examples, vocab):
     exs = []
     for input, num, preds, target in examples:
         exs.append((
-            beautify_text(" ".join([text_field.vocab.itos[inp]
-                                    if ind != num + 1 else "[[[" + text_field.vocab.itos[inp] + "]]]"
+            beautify_text(" ".join([vocab.itos[inp]
+                                    if ind != num + 1 else "[[[" + vocab.itos[inp] + "]]]"
                                     for ind, inp in enumerate(input)])),
             num,
-            [text_field.vocab.itos[p] for p in preds],
-            text_field.vocab.itos[target]
-        ))
-
-    logging.info(f'Current tops are ...')
-    logging.info(f'                    ... {accuracies}')
+            [vocab.itos[p] for p in preds],
+            vocab.itos[target]
+    ))
     for ex in exs:
         logging.info(f'                    ... {ex[0]}')
         logging.info(f'                    ... {ex[1]}')
         logging.info(f'                    ... {ex[2]}')
         logging.info(f'                    ... {ex[3]}')
         logging.info(f'===============================================')
-    mrr = MRR(*rnn_learner.predict_with_targs(True))
-    logging.info(f"mrr: {mrr}")
 
 
-def get_language_model(text_field, learning_mode=True, learning_rate_finding_mode = False):
-    model_name = nn_params["model_name"]
-    PATH_TO_MODEL= f'{nn_params["path_to_data"]}/{model_name}'
+def calc_and_display_top_k(rnn_learner, metric, vocab):
+    spl = metric.split("_")
+    cat_index = spl.index("cat")
+    if cat_index == -1 or len(spl) <= cat_index + 1:
+        raise ValueError(f'Illegal metric format: {metric}')
+    ks = list(map(lambda x: int(x), spl[1: cat_index]))
+    cat = int(spl[cat_index + 1])
+
+    accuracies, examples = top_k(*rnn_learner.predict_with_targs(True), ks, cat)
+
+    logging.info(f'Current tops are ...')
+    logging.info(f'                    ... {accuracies}')
+    if spl[-1] == 'show':
+        display_not_guessed_examples(examples, vocab)
+
+
+def calculate_and_display_metrics(rnn_learner, metrics, vocab):
+    for metric in metrics:
+        if metric.startswith("topk"):
+            calc_and_display_top_k(rnn_learner, metric, vocab)
+        elif metric == 'mrr':
+            mrr = MRR(*rnn_learner.predict_with_targs(True))
+            logging.info(f"mrr: {mrr}")
+
+
+def get_language_model(text_field):
+    model_name = nn_arch["model_name"]
+    PATH_TO_MODEL= f'{nn_arch["path_to_data"]}/{model_name}'
 
     TRAIN_CONTEXTS = f'{PATH_TO_MODEL}/train/contexts.src'
     TEST_CONTEXTS = f'{PATH_TO_MODEL}/test/contexts.src'
@@ -58,32 +77,32 @@ def get_language_model(text_field, learning_mode=True, learning_rate_finding_mod
     with open(TEST_CONTEXTS, 'r') as f:
         test_df = pandas.DataFrame([line for line in f])
 
-    languageModelData = LanguageModelData.from_dataframes(nn_params["path_to_data"], text_field, 0, train_df, test_df, test_df,
-                                                          bs=nn_params["bs"],
-                                                          bptt=nn_params["bptt"],
-                                                          min_freq=nn_params["min_freq"]
+    languageModelData = LanguageModelData.from_dataframes(nn_arch["path_to_data"], text_field, 0, train_df, test_df, test_df,
+                                                          bs=nn_arch["bs"],
+                                                          bptt=nn_arch["bptt"],
+                                                          min_freq=nn_arch["min_freq"]
                                                           # not important since we remove rare tokens during preprocessing
                                                           )
     pickle.dump(text_field, open(f'{PATH_TO_MODEL}/TEXT.pkl', 'wb'))
 
     logging.info(f'Dictionary size is: {len(text_field.vocab.itos)}')
 
-    opt_fn = partial(torch.optim.Adam, betas=nn_params['betas'])
+    opt_fn = partial(torch.optim.Adam, betas=nn_arch['betas'])
 
-    rnn_learner = languageModelData.get_model(opt_fn, nn_params['em_sz'], nn_params['nh'], nn_params['nl'],
-                                              dropouti=nn_params['drop']['outi'],
-                                              dropout=nn_params['drop']['out'],
-                                              wdrop=nn_params['drop']['w'],
-                                              dropoute=nn_params['drop']['oute'],
-                                              dropouth=nn_params['drop']['outh'])
-    rnn_learner.reg_fn = partial(seq2seq_reg, alpha=nn_params['reg_fn']['alpha'], beta=nn_params['reg_fn']['beta'])
-    rnn_learner.clip = nn_params['clip']
+    rnn_learner = languageModelData.get_model(opt_fn, nn_arch['em_sz'], nn_arch['nh'], nn_arch['nl'],
+                                              dropouti=nn_arch['drop']['outi'],
+                                              dropout=nn_arch['drop']['out'],
+                                              wdrop=nn_arch['drop']['w'],
+                                              dropoute=nn_arch['drop']['oute'],
+                                              dropouth=nn_arch['drop']['outh'])
+    rnn_learner.reg_fn = partial(seq2seq_reg, alpha=nn_arch['reg_fn']['alpha'], beta=nn_arch['reg_fn']['beta'])
+    rnn_learner.clip = nn_arch['clip']
 
     logging.info(rnn_learner)
 
     try:
         rnn_learner.load(model_name)
-        calculate_and_display_metrics(text_field)
+        calculate_and_display_metrics(rnn_learner, nn_params['metrics'], text_field.vocab)
     except FileNotFoundError:
         logging.warning(f"Model {model_name} not found. Training from scratch")
 
@@ -96,8 +115,8 @@ def get_language_model(text_field, learning_mode=True, learning_rate_finding_mod
         rnn_learner.sched.plot(path)
         logging.info(f"Plot is saved to {path}")
     elif nn_params['mode'] is Mode.TRAINING:
-        rnn_learner.fit(nn_params['lr'], n_cycle=nn_params['cycle']['n'], wds=nn_params['wds'],
-                        cycle_len=nn_params['cycle']['len'], cycle_mult=nn_params['cycle']['mult'],
+        rnn_learner.fit(nn_arch['lr'], n_cycle=nn_arch['cycle']['n'], wds=nn_arch['wds'],
+                        cycle_len=nn_arch['cycle']['len'], cycle_mult=nn_arch['cycle']['mult'],
                         metrics=[accuracy, f2, mrr_non_interactive])
 
         logging.info(f'Saving model: {model_name}')
@@ -116,7 +135,7 @@ def run_and_display_tests(m):
     print(text)
     print(beautify_text(text))
 
-    back_to_train_mode(m, nn_params['bs'])
+    back_to_train_mode(m, nn_arch['bs'])
 
 if __name__ =='__main__':
     logging.info("Using GPU: " + str(USE_GPU))
--- nn/params.py
+++ nn/params.py
@@ -10,21 +10,27 @@ class Mode(Enum):
 
 
 nn_params = {
-    'path_to_data': '../nn-data',
-    'model_name': 'few_data',
-    # 'model_name': 'devanbu_no_replaced_identifier_split',
-    'bs': 32,
-    'bptt': 70,
-    'em_sz': 300,  # size of each embedding vector
-    'nh': 400,     # number of hidden activations per layer
-    'nl': 3,       # number of layers
-    'min_freq': 5,
-    'betas': [0.7, 0.99],
-    'clip': 0.3,
-    'reg_fn': {'alpha': 2, 'beta': 1},
-    'drop': {'outi': 0.05, 'out': 0.05, 'w':0.1, 'oute': 0.02, 'outh': 0.05},
-    'lr': 1e-3, 'wds': 1e-6,
-    'cycle': {'n': 1, 'len': 1, 'mult': 2},
+    'arch': {
+        'path_to_data': '../nn-data',
+        'model_name': 'few_data',
+        # 'model_name': 'devanbu_no_replaced_identifier_split',
+        'bs': 32,
+        'bptt': 70,
+        'em_sz': 300,  # size of each embedding vector
+        'nh': 400,     # number of hidden activations per layer
+        'nl': 3,       # number of layers
+        'min_freq': 5,
+        'betas': [0.7, 0.99],
+        'clip': 0.3,
+        'reg_fn': {'alpha': 2, 'beta': 1},
+        'drop': {'outi': 0.05, 'out': 0.05, 'w':0.1, 'oute': 0.02, 'outh': 0.05},
+        'lr': 1e-3, 'wds': 1e-6,
+        'cycle': {'n': 1, 'len': 1, 'mult': 2}
+    },
+    'metrics': ['topk_1_10_100_cat_2_show', 'mrr'],
+    'testing': {
+
+    },
     'mode': Mode.TRAINING
 }
 
