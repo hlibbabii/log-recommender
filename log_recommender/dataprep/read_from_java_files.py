804427a2d84c2eeda7c059c501989b97e0d36274
hlibbabii <hlibbabii@gmail.com>
2018-07-11 08:43:09 +0200

    change read_from_java_files to make it paralelizable

--- log_recommender/dataprep/read_from_java_files.py
+++ log_recommender/dataprep/read_from_java_files.py
@@ -25,21 +25,18 @@ def read_file_contents(file_path):
             logging.error(f"Unicode decode error in file: {file_path}")
 
 
-def preprocess_and_write(dir, subdir, chunks):
-    files = [open(os.path.join(dir, f'context.{i}.src'), 'w') for i in range(chunks)]
-    total_files = sum([f for f in java_file_mapper(os.path.join(args.dir, subdir), lambda path: 1)])
-    print(f'Total amount of files to process: {total_files}')
+def preprocess_and_write(dir, subdir, chunk):
+    with (os.path.join(dir, f'context.{chunk}.src'), 'w') as f:
+        total_files = sum([f for f in java_file_mapper(os.path.join(args.dir, subdir, chunk), lambda path: 1)])
+        print(f'Total amount of files to process: {total_files}')
 
-    for ind, (lines_from_file, file_path) in enumerate(
-            java_file_mapper(os.path.join(args.dir, subdir), read_file_contents)):
-        if len(lines_from_file) > pp_params['more_lines_ignore']:
-            logging.debug(f"File {file_path} has {len(lines_from_file)} lines. Skiping...")
-            continue
-        logging.info(f"Processing file: {file_path} [{ind} out of {total_files}] containing {len(lines_from_file)} lines")
-        processed = apply_preprocessors(lines_from_file, pp_params["preprocessors"], {'interesting_context_words': []})
-        files[ind % chunks].write(processed)
-    for file in files:
-        file.close()
+        for lines_from_file, file_path in java_file_mapper(os.path.join(args.dir, subdir, chunk), read_file_contents):
+            if len(lines_from_file) > pp_params['more_lines_ignore']:
+                logging.debug(f"File {file_path} has {len(lines_from_file)} lines. Skiping...")
+                continue
+            logging.info(f"Processing file: {file_path} [{ind} out of {total_files}] containing {len(lines_from_file)} lines")
+            processed = apply_preprocessors(lines_from_file, pp_params["preprocessors"], {'interesting_context_words': []})
+            f.write(processed)
 
 
 if __name__ == '__main__':
@@ -47,6 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('--fastai-input-file', action='store',
                         default='../../nn-data/devanbu_no_replaced_identifier_split_no_tabs_under_2000_fixed/')
     parser.add_argument('--dir', action='store', default='../../../FSE\'17 Replication/projects/1_percent/')
+    parser.add_argument('--chunk', action='store')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
@@ -64,6 +62,6 @@ if __name__ == '__main__':
 
     with open(f'{dataset_dir}/params.json', 'w') as f:
         json.dump(pp_params, f)
-    preprocess_and_write(train_dir, "Train/", 10)
-    preprocess_and_write(test_dir, "Test/", 10)
+    preprocess_and_write(train_dir, "Train/", args.chunk)
+    preprocess_and_write(test_dir, "Test/", args.chunk)
 
