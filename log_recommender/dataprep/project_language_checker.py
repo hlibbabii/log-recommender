ffbb136cec76213776690f71b4ad35116f1da790
hlibbabii <hlibbabii@gmail.com>
2018-08-29 16:58:23 +0200

    project lang checker: enable passing thresholds as params; write verbose logs to a file

--- log_recommender/dataprep/project_language_checker.py
+++ log_recommender/dataprep/project_language_checker.py
@@ -6,8 +6,8 @@ from dataprep import base_project_dir
 from dataprep.lcsplitting.lowercase_words_splitter import load_english_dict
 from dataprep.preprocessors.noneng import isascii
 
-MIN_FREQ_TO_BE_NON_ENGLISH = 0.01
-MIN_WORDS_TO_BE_NON_ENGLISH = 5
+DEFAULT_MIN_FREQ_TO_BE_NON_ENG = 0.01
+DEFAULT_MIN_WORDS_TO_BE_NON_ENG = 5
 
 
 def create_word_to_lang_map(dicts_dir, english_general_dict):
@@ -49,20 +49,26 @@ def calc_lang_stats(file, word_to_lang_map):
 
 def check_more_than_limit(lang_to_percent, total):
     for v in lang_to_percent.values():
-        if v > MIN_FREQ_TO_BE_NON_ENGLISH and total * v > MIN_WORDS_TO_BE_NON_ENGLISH:
+        if v > min_freq_to_be_non_eng and total * v > min_words_to_be_non_eng:
             return True
     return False
 
 
 if __name__ == '__main__':
-    default_base_dataset_dir = f"{base_project_dir}/nn-data/new_framework/"
+    default_base_dataset_dir = f'{base_project_dir}/nn-data/test/'
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--base-dataset-dir', default=f'{default_base_dataset_dir}')
+    parser.add_argument('--min-freq', default=f'{DEFAULT_MIN_FREQ_TO_BE_NON_ENG}')
+    parser.add_argument('--min-words', default=f'{DEFAULT_MIN_WORDS_TO_BE_NON_ENG}')
     parser.add_argument('--base-dataset-dir', default=f'{default_base_dataset_dir}')
     parser.add_argument('preprocessed_dataset', help='path to preprocessed dataset relative '
                                                      'to the one passed as --base-dataset-dir param')
 
     args = parser.parse_args()
+
+    min_freq_to_be_non_eng = args.min_freq
+    min_words_to_be_non_eng = args.min_words
 
     path_to_dicts = f"{base_project_dir}/dicts/"
     path_to_non_eng_dicts = f"{path_to_dicts}/non-eng"
@@ -79,10 +85,16 @@ if __name__ == '__main__':
             calc_lang_stats(os.path.join(path_to_dir_with_preprocessed_projects, file), word_to_lang_map)
         non_eng = check_more_than_limit(lang_to_percent, total)
         if non_eng:
-            non_english_files.append(file)
+            non_english_files.append((file, lang_to_percent, total, lang_to_word_examples))
             print(f'Gen stats: file:{file}, {lang_to_percent}, total: {total}')
             print(lang_to_word_examples)
             print("\n\n")
-    with open(f'{path_to_dir_with_preprocessed_projects}/noneng_projects_{MIN_FREQ_TO_BE_NON_ENGLISH}_{MIN_WORDS_TO_BE_NON_ENGLISH}.txt', 'w') as f:
-        for file in non_english_files:
+    with open(f'{path_to_dir_with_preprocessed_projects}/noneng_projects_{min_freq_to_be_non_eng}_{min_words_to_be_non_eng}.txt', 'w') as f:
+        for file, _, _, _ in non_english_files:
             f.write(file + "\n")
+
+    with open(f'{path_to_dir_with_preprocessed_projects}/noneng_projects_verbose_{min_freq_to_be_non_eng}_{min_words_to_be_non_eng}.txt', 'w') as f:
+        for file, lang_to_percent, total, lang_to_word_examples in non_english_files:
+            f.write(f'Gen stats: file:{file}, {lang_to_percent}, total: {total}\n')
+            f.write(f"{lang_to_word_examples}\n")
+            f.write("\n\n")
