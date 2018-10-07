3d7d23295c5073c531f1fd9594862e2a686792fb
hlibbabii <hlibbabii@gmail.com>
2018-10-07 23:33:50 +0200

    [LM13]: fix loading dictionary for same case splitting for every time

--- logrec/dataprep/preprocessors/split.py
+++ logrec/dataprep/preprocessors/split.py
@@ -9,6 +9,30 @@ from dataprep.preprocessors.model.split import CamelCaseSplit, WithNumbersSplit,
     NonDelimiterSplitContainer, SameCaseSplit
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class SplittingDict(metaclass=Singleton):
+    def __init__(self, splitting_file_location):
+        self.splitting_dict = {}
+        start = time.time()
+        with open(splitting_file_location, 'r') as f:
+            for ln in f:
+                word, splitting = ln.split("|")
+                self.splitting_dict[word] = splitting.split()
+        logging.info(f"Splitting dictionary is build in {time.time()-start} s")
+
+
+def get_splitting_dictionary(splitting_file_location):
+    return SplittingDict(splitting_file_location).splitting_dict
+
+
 def camel_case(token_list, context):
     return [apply_splitting_to_token(identifier, split_string_camel_case) for identifier in token_list]
 
@@ -21,15 +45,7 @@ def with_numbers(token_list, context):
     return [apply_splitting_to_token(identifier, split_string_with_numbers) for identifier in token_list]
 
 def same_case(token_list, context):
-    splitting_file_location = context['splitting_file_location']
-    start = time.time()
-    splitting_dict = {}
-    with open(splitting_file_location, 'r') as f:
-        for ln in f:
-            word, splitting = ln.split("|")
-            splitting_dict[word] = splitting.split()
-    logging.info(f"Splitting dictionary is build in {time.time()-start} s")
-
+    splitting_dict = get_splitting_dictionary(context['splitting_file_location'])
     return [apply_splitting_to_token(identifier, partial(split_string_same_case, splitting_dict)) for identifier in token_list]
 
 
