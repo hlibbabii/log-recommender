ba9ba6cb064fb8e4d9bf47f3b63950b9e9ab14fd
hlibbabii <hlibbabii@gmail.com>
2018-10-16 09:50:17 +0200

    #6: get fastai-fork root to syspath

--- logrec/__init__.py
+++ logrec/__init__.py
@@ -4,3 +4,4 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../fastai-fork'))
