de78f7a03d972fbf3a95ba2207c1de0f4eeabdff
hlibbabii <hlibbabii@gmail.com>
2018-10-18 16:56:52 +0200

    try importing torch in the beginning

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 import pkgutil
 import unittest
@@ -7,6 +8,7 @@ from logrec.dataprep import base_project_dir
 
 class ImportsTest(unittest.TestCase):
     def test_imports(self):
+        importlib.import_module('torch')
         __all__ = []
         for loader, module_name, is_pkg in pkgutil.walk_packages([os.path.join(base_project_dir, "logrec")]):
             __all__.append(module_name)
