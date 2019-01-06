1727e41336a077ed9b3c22b8dcc2a5b1ffbd00bd
hlibbabii <hlibbabii@gmail.com>
2019-01-06 12:04:49 +0100

    #69: fix paths in travis properties

--- logrec/travis_properties.py
+++ logrec/travis_properties.py
@@ -5,10 +5,10 @@ base_project_dir = os.path.join(base_dir, 'hlibbabii', 'log-recommender')
 
 REWRITE_PARSED_FILE = False
 
-DEFAULT_RAW_DATASETS_DIR = os.path.join(base_dir, 'raw_datasets', 'devanbu', 'data')
-DEFAULT_PARSED_DATASETS_DIR = os.path.join(base_project_dir, 'nn-data', 'new_framework')
+DEFAULT_RAW_DATASETS_DIR = os.path.join(base_project_dir, 'nn-data', 'test', 'raw')
+DEFAULT_PARSED_DATASETS_DIR = os.path.join(base_project_dir, 'nn-data', 'test')
 
-DEFAULT_DATASET = 'en_100_percent'
+DEFAULT_DATASET = 'test1'
 
 DEFAULT_PARSE_PROJECTS_ARGS = []
 DEFAULT_PROJECT_LANGUAGE_CHECKER_ARGS = []
