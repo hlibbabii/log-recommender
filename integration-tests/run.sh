839250e12ef4a7144d8148688c95063b968f591a
hlibbabii <hlibbabii@gmail.com>
2019-01-09 21:01:19 +0100

    #56: add dispaying test directory contents after each step of integration tests pipeline

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -7,4 +7,7 @@ PROJECT_ROOT="$CURRENT_DIR/.."
 echo "doing cd to $PROJECT_ROOT"
 cd "$PROJECT_ROOT"
 
+echo "Running parse_projects.py ..."
 python logrec/dataprep/parse_projects.py test1
+echo "Showing contents of nn-data/test after parse_projects.py command:"
+tree -L 2 nn-data/test
