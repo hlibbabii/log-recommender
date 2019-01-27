4d2a6eeb21d5196a69a3bd61cbe877affceef92e
hlibbabii <hlibbabii@gmail.com>
2019-01-27 21:49:15 +0100

    use yaml config for logging

--- logrec/__init__.py
+++ logrec/__init__.py
@@ -1,11 +1,22 @@
+import yaml
+
+from logrec.properties import base_project_dir
+
 __author__ = 'hlib'
 
 import os
 import sys
 
 import logging
+import logging.config
 
-logging.basicConfig(level=logging.DEBUG)
+path = os.path.join(base_project_dir, 'logging.yaml')
+if os.path.exists(path):
+    with open(path, 'rt') as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+else:
+    logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('matplotlib').setLevel(logging.INFO)
 
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
