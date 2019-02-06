e49ff7d1cf6aaea46337e9a4199800d7b75ebc52
hlibbabii <hlibbabii@gmail.com>
2019-02-06 13:16:59 +0100

    print -> logger

--- logrec/properties.py
+++ logrec/properties.py
@@ -1,11 +1,14 @@
+import logging
 import os
 
+logger = logging.getLogger(__name__)
+
 if os.environ['HOME'] == '/home/hlib':
-    print(f'$HOME is {os.environ["HOME"]}, using local_properties.py')
+    logger.info(f'$HOME is {os.environ["HOME"]}, using local_properties.py')
     from logrec.local_properties import *
 elif os.environ['HOME'] == '/home/travis':
-    print(f'$HOME is {os.environ["HOME"]}, using travis_properties.py')
+    logger.info(f'$HOME is {os.environ["HOME"]}, using travis_properties.py')
     from logrec.travis_properties import *
 else:
-    print(f'$HOME is {os.environ["HOME"]}, using vsc_properties.py')
+    logger.info(f'$HOME is {os.environ["HOME"]}, using vsc_properties.py')
     from logrec.vsc_properties import *
