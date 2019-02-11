5df8d8d91d66c477809b599b95de407580fd3042
hlibbabii <hlibbabii@gmail.com>
2019-02-11 13:44:15 +0100

    add script for splitting identifiers

---
+++ logrec/dataprep/split_identifiers.py
@@ -0,0 +1,46 @@
+import os
+
+from logrec import base_project_dir
+from logrec.cli import preprocess
+from logrec.dataprep.prepconfig import PrepParam, PrepConfig
+
+prep_configs = [
+    '00000',
+    '11111',
+    '00200',
+    '00400',
+    '00500',
+    '00600',
+    '10400',
+    '10500',
+    '10600'
+]
+
+path_to_file = os.path.join(base_project_dir, 'identifiers')
+path_to_file_out = os.path.join(base_project_dir, 'identifiers.csv')
+
+DELIMITER = ','
+
+
+def gen():
+    with open(path_to_file, 'r') as f:
+        identifiers = [line.rstrip('\n') for line in f]
+
+    csv_lines = [DELIMITER.join(["config"] + [p for p in PrepParam] + identifiers)]
+    for prep in prep_configs:
+        csv_line = [prep]
+        for p in PrepParam:
+            csv_line.append(
+                PrepConfig.human_readable_values[p][PrepConfig.from_encoded_string(prep).get_param_value(p)])
+        for identifier in identifiers:
+            tokens = preprocess(identifier, prep)
+            csv_line.append(' '.join(tokens))
+        csv_lines.append(DELIMITER.join(csv_line))
+
+    with open(path_to_file_out, 'w') as f:
+        for line in csv_lines:
+            f.write(f'{line}\n')
+
+
+if __name__ == '__main__':
+    gen()
