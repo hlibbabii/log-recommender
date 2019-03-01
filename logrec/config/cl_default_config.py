092ab1ca6e0a9ddb66607cb3ce4cbd553c7802c5
Hlib <hlibbabii@gmail.com>
2019-03-01 17:29:23 +0100

    update classifier config

--- logrec/config/cl_default_config.py
+++ logrec/config/cl_default_config.py
@@ -4,8 +4,8 @@ from logrec.config.model import Droupouts, RegFn, Cycle, Data, Arch, \
     PretrainingType, LRS, ClassificationType, ClassifierTesting
 
 data = Data(
-    dataset=DEFAULT_DATASET,
-    repr='10411',
+    dataset='nodup_en_only',
+    repr='324101',
     percent=100,
     start_from=0,
     backwards=False
@@ -14,11 +14,11 @@ data = Data(
 arch = Arch(
     bidir=False,
     qrnn=False,
-    bs=32,
+    bs=64,
     validation_bs=32,
     bptt=990,
-    em_sz=150,  # size of each embedding vector
-    nh=300,  # number of hidden activations per layer
+    em_sz=300,  # size of each embedding vector
+    nh=650,  # number of hidden activations per layer
     nl=3,  # number of layers
     adam_betas=[0.7, 0.99],
     clip=25.,
@@ -27,27 +27,27 @@ arch = Arch(
 )
 
 classifier_config = ClassifierConfig(
-    base_model=None,
-    pretraining_type=None,
+    base_model='100_baseline_extratrained_extratrained',
+    pretraining_type=PretrainingType.ONLY_ENCODER,
     data=data,
     arch=arch,
     classification_type=ClassificationType.LOCATION,
-    min_log_coverage_percent=1.0,
+    min_log_coverage_percent=5.0,
     training=ClassifierTraining(
         lrs=LRS(
             base_lr=1e-3,
-            factor=1.0,
+            factor=2.6,
             multipliers=[4, 3, 2.5, 1, 0]
         ),
         wds=1e-6,
         stages=[
             # Stage(-1, Cycle(len=0, n=1, mult=1)),
             # Stage(-2, Cycle(len=0, n=1, mult=1)),
-            Stage(0, Cycle(len=4, n=1, mult=2))
+            Stage(0, Cycle(len=1, n=1, mult=2))
         ],
         early_stop=True
 
     ),
     metrics=['accuracy', 'mrr'],
-    testing=ClassifierTesting(n_samples=200)
+    testing=ClassifierTesting(n_samples=20)
 )
